e1f3e1bbdff820d9c810fd055d56ae955ad55c71
guanxin2015 <124632303@qq.com>
2024-12-16 23:34:49 +0800

    add shenhe

--- app_server/vaildate/vaildate_target.py
+++ app_server/vaildate/vaildate_target.py
@@ -1,11 +1,36 @@
-
+import json
+from config import Config
+from app_server.utils.coze import run_coze_workflow
 # 文本内容安全识别
 # https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/sec-center/sec-check/msgSecCheck.html
 
 
-def validate_target_data(data):
-    if not data.get('title'):
-        return '目标标题不能为空'
 
+def validate_target_data(data):
+    title = data.get('title')
+    if not title:
+        return '目标标题不能为空'
         
+    # 调用Coze工作流
+    parameters = {
+            "input": title
+    }
+    result = run_coze_workflow(Config.WorkFlowID_ZFSH, parameters)
+        
+    if result['code'] != 0:
+        return (f"Coze工作流调用失败: {result['msg']}")
+    try:
+        data_dict = json.loads(result['data'])   
+           
+    except json.JSONDecodeError as e:
+        return (f"Coze工作流返回数据格式错误: {e}")  
+    # 解析返回的数据
+    res = data_dict['result']
+    print('res',res,type(res))
+    if res == False:
+        reason  = data_dict['reason']
+        if reason:
+            return reason
+        else:
+            return '审核没通过'      
     return None 
